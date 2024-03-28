e12d8b872eb3d3dda0ed91bf95c3df75bd68354a
Sam Cheng <56993697+samchenghowing@users.noreply.github.com>
2024-03-28 19:42:07 +0800

    https

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    # app.config['SESSION_COOKIE_SECURE'] = True
+    # app.config['SESSION_COOKIE_HTTPONLY'] = True
+    socketio = SocketIO(app, certfile='path/to/cert.pem', keyfile='path/to/key.pem')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
