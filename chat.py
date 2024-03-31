e7b7a4cf152436b90c81f6e78f61054fefb59b23
Sam Cheng <samchenghowing@gmail.com>
2024-03-31 16:35:09 +0800

    add self signed key and cert for ssl connection

--- chat.py
+++ chat.py
@@ -4,4 +4,5 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # as it is self-signed certificates the browser will generate a security warning
+    socketio.run(app, ssl_context=('cert.pem', 'key.pem'))
